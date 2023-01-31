591360e90c13aa2e0f308286f1531cfd377906fd
thegreatrussia <123950939+thegreatrussia@users.noreply.github.com>
2023-01-31 20:53:17 +0900

    Update flood.py

--- flood.py
+++ flood.py
@@ -11,19 +11,20 @@ force = ForceSensor('D')
 init_required = False
 is_up = False
 
-distance.light_up_all() # 거리 센서 불 킴
+distance.light_up_all() # горит датчик расстояния!
 
 while True:
     if init_required == True:
-        print("initing something cool...")
+        print("я запускаю Hub.... он будет завершен в любые секунды!")
         distance.light_up_all(100)
         init_required = False
+        print("Привет, Пользователь!")
     
     if force.is_pressed() and force.get_force_newton()!=None:
         if is_up == False:
             print("Ignore")
         elif force.get_force_newton() >= 5:
-            print("Going Down Suckers!")
+            print("Я спускаюсь, привет земля!")
             for i in range(0,7):
                 hub.speaker.beep(84, 0.5)
                 motor_pair_odin.move(1, 'cm', -100, 100)
@@ -34,6 +35,7 @@ while True:
     print(distance.get_distance_cm())
     if distance.get_distance_cm()!=None:
         if distance.get_distance_cm() <= 20:
+            print("Я поднимаюсь! Здравствуй, Голубое Небо!")
             distance.light_up_all(0)
             for i in range(0,3):
                 hub.speaker.beep(84, 0.5)
